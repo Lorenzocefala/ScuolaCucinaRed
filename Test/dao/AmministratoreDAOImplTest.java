0d57330fcbbf4dbb8c9b6fb1754470199b61efdb
Martina Picozzi <martinafrancesca.picozzi1@gmail.com>
2020-02-10 14:00:49 +0100

    Fix info.properties + test

--- Test/dao/AmministratoreDAOImplTest.java
+++ Test/dao/AmministratoreDAOImplTest.java
@@ -4,11 +4,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import entity.Utente;
+
 class AmministratoreDAOImplTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test() throws Exception {
+		AmministratoreDAO dao= new AmministratoreDAOImpl();
+		Utente u = new Utente("aa","aa","aa","aa", new java.util.Date(),"pp","pp", true);
+		dao.insert(u);
+//		u.setCognome("Doria");
+//		dao.delete("aa");
+//		dao.update(u);
+//		System.out.println(dao.select("marco81"));
 	}
 
 }
