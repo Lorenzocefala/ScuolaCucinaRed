d1ac74e99ea9e859984c232d2589ac60dca185a0
Martina Picozzi <martinafrancesca.picozzi1@gmail.com>
2020-02-10 15:29:34 +0100

    Test junit

--- Test/dao/AmministratoreDAOImplTest.java
+++ Test/dao/AmministratoreDAOImplTest.java
@@ -4,15 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import entity.Utente;
+// import entity.Utente;
 
 class AmministratoreDAOImplTest {
 
 	@Test
 	void test() throws Exception {
+		Utente u = new Utente("2","password","aa","aa", new java.util.Date(01/11/1997),"pp@gmail.com","123456789", true);
+		try {
 		AmministratoreDAO dao= new AmministratoreDAOImpl();
-		Utente u = new Utente("aa","aa","aa","aa", new java.util.Date(),"pp","pp", true);
 		dao.insert(u);
+		dao.select("2");
+		assertThat (u, is ("2","password","aa","aa", new java.util.Date(01/11/1997),"pp@gmail.com","123456789", true));
+		} catch (Exception ex) {
+			fail ("UEx" + ex.getMessage());
+		}
 //		u.setCognome("Doria");
 //		dao.delete("aa");
 //		dao.update(u);
